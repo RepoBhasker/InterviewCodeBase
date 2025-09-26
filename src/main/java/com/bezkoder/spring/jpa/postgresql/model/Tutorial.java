08828e58770319b7a3515ec07432a75369c08b1b
bhaskerreddy <bhasker4191@gmail.com>
2025-09-26 17:50:32 +0530

    git commit -m "first commit"

---
+++ src/main/java/com/bezkoder/spring/jpa/postgresql/model/Tutorial.java
@@ -0,0 +1,79 @@
+package com.bezkoder.spring.jpa.postgresql.model;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "tutorials")
+public class Tutorial {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.AUTO)
+  private long id;
+
+  @Column(name = "title")
+  private String title;
+
+  @Column(name = "description")
+  private String description;
+
+  @Column(name = "published")
+  private boolean published;
+
+  @Column(name = "create_date")
+  private String createDate;
+
+  public Tutorial(String title, String description, String createDate, boolean published) {
+    this.title = title;
+    this.description = description;
+    this.published = published;
+    this.createDate = createDate;
+  }
+
+
+  public long getId() {
+    return id;
+  }
+
+  public String getTitle() {
+    return title;
+  }
+
+  public void setTitle(String title) {
+    this.title = title;
+  }
+
+  public String getDescription() {
+    return description;
+  }
+
+  public void setDescription(String description) {
+    this.description = description;
+  }
+
+  public boolean isPublished() {
+    return published;
+  }
+
+  public void setPublished(boolean isPublished) {
+    this.published = isPublished;
+  }
+
+  public String getcreateDate() {
+    return createDate;
+  }
+
+  public void setcreateDate(String createDate) {
+    this.createDate = createDate;
+  }
+
+  @Override
+  public String toString() {
+    return "Tutorial{" +
+            "id=" + id +
+            ", title='" + title + '\'' +
+            ", description='" + description + '\'' +
+            ", published=" + published +
+            ", createDate='" + createDate + '\'' +
+            '}';
+  }
+}
